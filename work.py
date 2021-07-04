03d4fff6da62ca2dcb5a962130cdd1e617b1a544
SoorejRB <soorejrb@gmail.com>
2021-07-04 12:21:25 +0530

    my practice

---
+++ work.py
@@ -0,0 +1,150 @@
+a = 20
+b = 20.0
+c = ""
+'''if a == b:
+    print("a = b")
+else:
+    print("a != b")'''
+
+'''x= 10
+ y= 15
+ z= 100
+if x == z:
+    print("x equals z")
+elif x == y:
+    print("x equals y")
+elif z == k:
+    print("z equals k")
+else:
+    print("x not equals z or y")'''
+
+'''k= ""
+l= []
+p= 2
+if k:
+    print("k is true")
+elif l:
+    print("l is true")
+elif p:
+    print("p is true")
+else:
+    print("k l p are false")'''    
+
+'''l = str(10)
+for i in l:
+    print(i)'''
+    
+'''a = 30
+while a > 20:
+    print(a)
+    a = a-1'''
+
+'''a=340
+while a == 340:
+    a = a-1
+
+    print(a)'''
+
+
+'''v = ["c","m","l"]
+w = [25,36,96]'''
+
+'''for i in v:
+    print(i)
+    for i in w:
+        print(i)'''
+'''a = 20
+b = 20.0
+c = ["a","s","d","f"]
+d = [52,63,65]
+
+for i in c:
+    if i == "a":
+        continue
+    if i == "f":
+        break
+    print(i)'''
+
+'''if 1:
+    print("hello")
+    pass'''
+w = [25,36,96]
+'''try:
+    print(w[3])
+except:
+    print(w[2])'''
+
+'''try:
+    print(w[3])
+except:
+    print("error")
+    print(w[0])
+else:
+    print("no exeption")
+finally:
+    print("ok")'''
+
+'''def sum(a,b):
+    print(a,b)
+    c  = a+b
+    return c'''
+'''def abc(x =1,y=2):
+    print(x,y)
+    z = x + y
+    return z
+m = abc(3,4)
+print(m)'''
+
+'''def arun():
+    z = 0
+    for x in range(5):
+        yield z
+        z += 2
+        
+y = arun()
+print(y.__next__())
+print(y.__next__())
+print(y.__next__())
+print(y.__next__())
+print(y.__next__())
+print(y.__next__())
+print(y.__next__())
+print(y.__next__())'''
+
+def numbers():
+    return 1
+x = numbers()
+print(x)
+
+
+
+
+
+
+
+
+
+          
+          
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    
+    
