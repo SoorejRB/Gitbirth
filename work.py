e5df22cbc6324e5f8914201759e96ef375d0a4de
SoorejRB <86906234+SoorejRB@users.noreply.github.com>
2021-07-04 12:44:40 +0530

    class

--- work.py
+++ work.py
@@ -1,120 +1,166 @@
-a = 20
-b = 20.0
-c = ""
-'''if a == b:
-    print("a = b")
-else:
-    print("a != b")'''
+#classes
 
-'''x= 10
- y= 15
- z= 100
-if x == z:
-    print("x equals z")
-elif x == y:
-    print("x equals y")
-elif z == k:
-    print("z equals k")
-else:
-    print("x not equals z or y")'''
+#class MyNewClass():
+#    '''my new class, doc string'''
 
-'''k= ""
-l= []
-p= 2
-if k:
-    print("k is true")
-elif l:
-    print("l is true")
-elif p:
-    print("p is true")
-else:
-    print("k l p are false")'''    
+#    pass
 
-'''l = str(10)
-for i in l:
-    print(i)'''
-    
-'''a = 30
-while a > 20:
-    print(a)
-    a = a-1'''
-
-'''a=340
-while a == 340:
-    a = a-1
-
-    print(a)'''
+#x = MyNewClass()
+#print(x.__doc__)
 
 
-'''v = ["c","m","l"]
-w = [25,36,96]'''
+class MyNewClass():
+    '''my new class, doc string'''
+    b = 30
+    def __init__(self):
+        a = 10
+        print(a)
+        print(self.b)
+        self.b = 15
 
-'''for i in v:
-    print(i)
-    for i in w:
-        print(i)'''
-'''a = 20
-b = 20.0
-c = ["a","s","d","f"]
-d = [52,63,65]
-
-for i in c:
-    if i == "a":
-        continue
-    if i == "f":
-        break
-    print(i)'''
-
-'''if 1:
-    print("hello")
-    pass'''
-w = [25,36,96]
-'''try:
-    print(w[3])
-except:
-    print(w[2])'''
-
-'''try:
-    print(w[3])
-except:
-    print("error")
-    print(w[0])
-else:
-    print("no exeption")
-finally:
-    print("ok")'''
-
-'''def sum(a,b):
-    print(a,b)
-    c  = a+b
-    return c'''
-'''def abc(x =1,y=2):
-    print(x,y)
-    z = x + y
-    return z
-m = abc(3,4)
-print(m)'''
-
-'''def arun():
-    z = 0
-    for x in range(5):
-        yield z
-        z += 2
+    def hello(self, v):
+        print(v)
+        print(self.b)
         
-y = arun()
-print(y.__next__())
-print(y.__next__())
-print(y.__next__())
-print(y.__next__())
-print(y.__next__())
-print(y.__next__())
-print(y.__next__())
-print(y.__next__())'''
+'''
+y = MyNewClass()
+print(y.b)
+print(MyNewClass.b)
+print(y.b)
+'''
+'''
+z = MyNewClass()
+z.hello(2)
+z.b = 90
+z.hello(3)
+'''
 
-def numbers():
-    return 1
-x = numbers()
-print(x)
+#inheritance
+
+#single inheritance
+class H(MyNewClass):
+    def __init__(self):
+        print(self.b)
+        
+    def hello(self, v):
+        print(v+1)
+        print(self.b+1)
+        return super().hello(v)
+'''
+x = H()
+x.hello(55)
+'''
+#multilevel inheritance
+
+class Multi(H):
+    def __init__(self):
+        print(self.b)
+'''
+y = Multi()
+'''
+#multiple inheritance
+class Ref():
+    k = 33
+    pass
+
+
+class Multiple(MyNewClass, Ref):
+    def __init__(self):
+        print(self.b)
+'''        print(self.k)
+m = Multiple()
+'''
+#hierarchical inheritance
+class Hierar(Ref):
+    pass
+
+
+class NHier(Ref):
+    pass
+
+#hybrid inheritance
+class A:
+    d = 56
+
+class B(A):
+    pass
+
+class C(A):
+    pass
+
+class D(B, C):
+    pass
+
+z = D()
+print(z.d)
+        
+
+
+
+#eg:
+class ClassRoom():
+    class_name = 10
+    class_teacher = "sam"
+    is_digital = True
+
+class Student1(ClassRoom):
+    student_name = "alex"
+    student_id = 202
+
+class Student2(ClassRoom):
+    student_name = "alok"
+    student_id = 101   
+
+
+l1 = Student1()
+
+print(l1.class_name)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
